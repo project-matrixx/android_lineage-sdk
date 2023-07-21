5e93692d09c30a6692503f2369b89f777ec673e7
mukesh22584 <mks22584@gmail.com>
2023-07-21 16:32:09 +0530

    sdk: Initial Matrixx conversion
    Change-Id: Iaaacc870b2d16bf4f29391c5065851729a51554f

--- sdk/src/java/lineageos/os/Build.java
+++ sdk/src/java/lineageos/os/Build.java
@@ -17,10 +17,10 @@ public class Build {
     public static final String UNKNOWN = "unknown";
 
     /** A build ID utilized to distinguish lineageos versions */
-    public static final String CRDROID_VERSION = getString("ro.crdroid.version");
+    public static final String CRDROID_VERSION = getString("ro.matrixx.version");
 
     /** A build ID string meant for displaying to the user */
-    public static final String CRDROID_DISPLAY_VERSION = getString("ro.crdroid.display.version");
+    public static final String CRDROID_DISPLAY_VERSION = getString("ro.matrixx.display.version");
 
     private static final SparseArray<String> sdkMap;
     static
